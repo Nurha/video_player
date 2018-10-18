2539d05d6ccbbf12746caa2b8afa23ea7792c2f8
Nurha <nurha@projectcodex.co>
2018-10-18 21:00:15 +0200

    add toggle for play button and skipping function for skipping back or ahead.

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// get the elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// build out functions
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    toggle.textContent = this.paused ? '►' : '❚ ❚';
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip)
+}
+
+// hook up the event listerners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
